c995f98bcdf494d6115c8c13313dfc5a4ffd70df
Viona Kaleb <vionakaleb@gmail.com>
2021-06-22 12:17:43 +0700

    Initial

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,68 @@
+import { lazy } from "react";
+import IntroContent from "../../content/IntroContent.json";
+import MiddleBlockContent from "../../content/MiddleBlockContent.json";
+import AboutContent from "../../content/AboutContent.json";
+import MissionContent from "../../content/MissionContent.json";
+import ProductContent from "../../content/ProductContent.json";
+import ContactContent from "../../content/ContactContent.json";
+import IgooanaLogo from "../../content/igooana_logo.png";
+
+const Contact = lazy(() => import("../../components/ContactForm"));
+const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
+const Container = lazy(() => import("../../common/Container"));
+const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
+const ContentBlock = lazy(() => import("../../components/ContentBlock"));
+
+const Home = () => {
+  return (
+    <Container>
+      <ScrollToTop />
+      <ContentBlock
+        type="right"
+        title={IntroContent.title}
+        picture={IgooanaLogo}
+        content={IntroContent.text}
+        button={IntroContent.button}
+        icon="developer.svg"
+        id="intro"
+      />
+      <MiddleBlock
+        title={MiddleBlockContent.title}
+        content={MiddleBlockContent.text}
+        button={MiddleBlockContent.button}
+      />
+      <ContentBlock
+        type="left"
+        title={AboutContent.title}
+        picture={"none"}
+        content={AboutContent.text}
+        section={AboutContent.section}
+        icon="graphs.svg"
+        id="about"
+      />
+      <ContentBlock
+        type="right"
+        title={MissionContent.title}
+        picture={"none"}
+        content={MissionContent.text}
+        icon="product-launch.svg"
+        id="mission"
+      />
+      <ContentBlock
+        type="left"
+        title={ProductContent.title}
+        picture={"none"}
+        content={ProductContent.text}
+        icon="waving.svg"
+        id="product"
+      />
+      <Contact
+        title={ContactContent.title}
+        content={ContactContent.text}
+        id="contact"
+      />
+    </Container>
+  );
+};
+
+export default Home;
